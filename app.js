136545d6b9a43f8814dc82a1c1376a1842e17db1
Eric Jacobson <ericchristianjacobson@gmail.com>
2022-03-22 14:13:24 -0400

    Full pipeline test, buddy

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Scary World!'));
+app.get('/', (req, res) => res.send('Hello Scary World Theory!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
